509b03923650e17bcec4fbfbe580e5ee9036b780
FortressZero <joshualee430@gmail.com>
2023-01-20 23:23:00 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#bot.py
+import os
+
+# plans
+# have bot respond automatically to IMAGES
+# have bot determine if image is nsfw, if so, mark it as nsfw. 
+# (optional?) have bot determine if discord OP wants to approve (settings?)
+# push to reddit api that makes post
+
