2f38f501e3e6eb0155a464b1250fe2df638e27a2
FortressZero <joshualee430@gmail.com>
2023-01-24 02:23:14 -0500

    help commands + image detection

--- disc2redd.py
+++ disc2redd.py
@@ -22,3 +22,28 @@ async def on_ready():
 async def on_command_error(ctx, error):
     await ctx.send("Sorry, I do not recognize that command!")
     
+@bot.event 
+async def on_guild_join(guild):
+    print(f"I have joined {guild}!")
+    
+@bot.event
+async def ping(ctx):
+    await ctx.send(f"pong! connection speed is {round(bot.latency * 1000)} ms")
+
+@bot.command(name='help')
+async def help(ctx):
+    embed=discord.Embed(title="Help Commands", 
+    description="""Hey! This is Discord2Reddit bot!
+This bot is currently in progress! More will be added soon.""", 
+    color=0xfd5bfd)
+    await ctx.send(embed=embed)
+
+# image detection and upload
+image_exts = ["png", "jpeg", "jpg", "gif", "jpg", "mp4", "mov"]
+@bot.event
+async def on_message(message: discord.Message):
+    for attachment in message.attachments:
+        if any(attachment.filename.lower().endswith(image) for image in image_exts):
+            await attachment.save(f'attachments/{attachment.filename}')
+
+bot.run(TOKEN)
