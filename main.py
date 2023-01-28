5525d44bcd3e0e35b2447cfeb3b8073966207963
FortressZero <joshualee430@gmail.com>
2023-01-27 21:01:02 -0500

    reddit acc created

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import os
 
 # plans
 # have bot respond automatically to IMAGES
-# have bot determine if image is nsfw, if so, mark it as nsfw. 
+# have bot determine if image is nsfw, if so, mark it as nsfw. (optional?)
 # (optional?) have bot determine if discord OP wants to approve (settings?)
 # push to reddit api that makes post
+
+# type of reddit api for future reference: 
+# debating between script and web app...
+# script will work fine but will not scale too well (but does it have to?)
+# web app will scale better since it will allow users to login into their own reddit accs
+# for now, I will be developing the reddit api as a SCRIPT but future proof it 
+# so it can be turned into a WEB APP later
 
